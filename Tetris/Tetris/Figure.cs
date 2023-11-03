bcd65e7218732c657eb95ee7d0387092d639aa17
ttruefix14 <ttruefix14@gmail.com>
2023-11-03 16:35:57 +0300

    Добавил родительский класс Figure

---
+++ Tetris/Tetris/Figure.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    public class Figure
+    {
+        public Point[] points = new Point[4];
+
+        public void Draw()
+        {
+            foreach (Point p in points)
+            {
+                p.Draw();
+            }
+
+        }
+    }
+}
