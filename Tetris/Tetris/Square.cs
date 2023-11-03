0c44b953a6708a3c4f45149e54de4410578a8be1
ttruefix14 <ttruefix14@gmail.com>
2023-11-03 12:19:48 +0300

    Исправил класс Square по домашнему заданию

--- Tetris/Tetris/Square.cs
+++ Tetris/Tetris/Square.cs
@@ -12,18 +12,17 @@ namespace Tetris
         
         public Square(int x, int y,  char c) 
         {
-            points[0] = new Point(x, x, c);
-            points[1] = new Point(x, y, c);
-            points[2] = new Point(y, x, c);
-            points[3] = new Point(y, y, c);
+            points[0] = new Point(x, y, c);
+            points[1] = new Point(x + 1, y, c);
+            points[2] = new Point(x, y + 1, c);
+            points[3] = new Point(x + 1, y +  1, c);
         }
 
         public void Draw()
         {
             foreach(Point p in points)
             {
-                Console.SetCursorPosition(p.x, p.y);
-                Console.Write(p.c);
+                p.Draw();
             }
 
         }
