bdf09f52ac591b72a78f1e2d470675ec3075cacb
ttruefix <ttruefix14@gmail.com>
2023-11-04 18:06:17 +0300

    Добавил класс Direction, добавил метод Move в Figure

---
+++ Tetris/Enums/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tetris;
+
+namespace Enums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Square square = new Square(5, 5, '#');
+
+            square.Draw();
+
+            while (true)
+            {
+                ConsoleKey key = Console.ReadKey().Key;
+                if(!square.consoleMoves.ContainsKey(key)) {
+                    break;
+                }
+                square.Move(square.consoleMoves[key]);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
