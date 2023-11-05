9c1d9669b3f973bd7e2fc1c64bc9a3da33792d54
ttruefix <ttruefix14@gmail.com>
2023-11-05 20:41:13 +0300

    Добавил класс FigureGenerator

---
+++ Tetris/Randomizer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Randomizer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int value = random.Next(0, 5);
+            Console.WriteLine(value);
+
+            Console.ReadLine();
+        }
+    }
+}
